379c04b4b928f6ca2e09512d431fb9684f14c606
narriteration <narriteration@gmail.com>
2017-01-15 22:24:34 -0800

    commented out all possible combinations of cards in js file

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,25 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "queen";
 
+/*
+I don't understand the directions for Assignment 8.
+Am I being asked to write out the javascript expressions that compare all possible combinations?
+Or to write code that works to solve the game?!
+
+cardOne == cardTwo;
+cardOne == cardThree;
+card0ne == cardFour;
+
+cardTwo == cardOne;
+cardTwo == cardThree;
+cardTwo == cardFour;
+
+cardThree == card0ne;
+cardThree == cardTwo;
+cardThree == cardFour;
+
+cardFour == cardOne;
+cardFour == cardTwo;
+cardFour == cardThree;
+*/
+
