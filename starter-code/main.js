10abddb9cf44e8b225a96b81b6ffc88c5b49570b
narriteration <narriteration@gmail.com>
2017-01-17 08:09:22 -0800

    add comments to main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,7 +5,19 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "queen";
 
+
+
 /*
+
+TRIED TO PUSH TO GITHUB
+FATAL ERROR: INVALID GITHUB USERNAME / PASSWORD
+I THINK FROM WHEN I JUST CHANGED MY GITHUB HANDLE
+LOOK ONLINE TO FIX! :D
+
+NEED TO FIX BEFORE PROCEEDING 
+
+
+
 I don't understand the directions for Assignment 8.
 Am I being asked to write out the javascript expressions that compare all possible combinations?
 Or to write code that works to solve the game?!
