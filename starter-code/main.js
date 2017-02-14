d089ffbda09172eb9a24b89f167401f0be16fd3b
narriteration <narriteration@gmail.com>
2017-02-14 09:19:02 -0800

    practicing git commits

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,7 +1,7 @@
 console.log("JS file is connected to HTML! Woo! Says Chelsea")
 
 var cardOne = "queen";
-var cardTwo = "queen";
+var cardTwo = "king";
 var cardThree = "king";
 var cardFour = "queen";
 
