927bf31f3c61e63d309bae9f8e6d72603d4048a5
narriteration <narriteration@gmail.com>
2017-02-14 11:43:27 -0800

    added game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,41 +1,29 @@
 console.log("JS file is connected to HTML! Woo! Says Chelsea")
 
 var cardOne = "queen";
-var cardTwo = "king";
+var cardTwo = "queen";
 var cardThree = "king";
-var cardFour = "queen";
+var cardFour = "king";
+
+if (cardOne === cardTwo || cardThree === cardFour) {
+	alert ("You found a match!")
+} else {
+	alert ("Sorry, try again.")
+}
 
 
 
 /*
 
-TRIED TO PUSH TO GITHUB
-FATAL ERROR: INVALID GITHUB USERNAME / PASSWORD
-I THINK FROM WHEN I JUST CHANGED MY GITHUB HANDLE
-LOOK ONLINE TO FIX! :D
+1) In your main.js file, create an if else statement that checks for equality between two of your cards 
+(you choose which ones).
 
-NEED TO FIX BEFORE PROCEEDING 
+An if statement should consist of a Boolean checking for equality between two created variables. 
+You should have a condition to compare two "king" variables, two "queen" variables, and one of each. 
+Start by comparing cardTwo with cardFour (using ===).
 
+2) If values are equal, execute an alert with the message "You found a match!" 
+If values are not equal, execute an alert with the message "Sorry, try again."
 
-
-I don't understand the directions for Assignment 8.
-Am I being asked to write out the javascript expressions that compare all possible combinations?
-Or to write code that works to solve the game?!
-
-cardOne == cardTwo;
-cardOne == cardThree;
-card0ne == cardFour;
-
-cardTwo == cardOne;
-cardTwo == cardThree;
-cardTwo == cardFour;
-
-cardThree == card0ne;
-cardThree == cardTwo;
-cardThree == cardFour;
-
-cardFour == cardOne;
-cardFour == cardTwo;
-cardFour == cardThree;
 */
 
