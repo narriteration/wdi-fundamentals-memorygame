ef538a73d5df7cd2d300ce101645f18ce51b5625
narriteration <narriteration@gmail.com>
2017-02-16 12:20:53 -0800

    completed assignment 10 successfully: four cards appear from js code

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,25 +5,42 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+
+// 4. Create a function called createCards in your JavaScript file. 
+// This function will contain the code you just wrote to generate your cards
+var createCards = function() {
+
+	// 1. get element with id of game-board and set it to a variable
+	var gameBoard = document.getElementById('game-board');
+
+	// 2. create a for loop that creates an HTML element for each card
+	for (var i = 0; i < 4; i++) {
+
+		// 2.5 Each HTML element should be a div 
+		var div = document.createElement('div');
+		// and have the class card
+		div.className = 'card';
+		// 3. Append each newly created card to the div with the ID game-board.
+		gameBoard.appendChild(div);
+	}
+}
+
+// 5. call the function (make it happen!)
+createCards();
+
+
+/*
 if (cardOne === cardTwo || cardThree === cardFour) {
 	alert ("You found a match!")
 } else {
 	alert ("Sorry, try again.")
 }
+*/
+
 
 
 
 /*
-
-1) In your main.js file, create an if else statement that checks for equality between two of your cards 
-(you choose which ones).
-
-An if statement should consist of a Boolean checking for equality between two created variables. 
-You should have a condition to compare two "king" variables, two "queen" variables, and one of each. 
-Start by comparing cardTwo with cardFour (using ===).
-
-2) If values are equal, execute an alert with the message "You found a match!" 
-If values are not equal, execute an alert with the message "Sorry, try again."
 
 */
 
